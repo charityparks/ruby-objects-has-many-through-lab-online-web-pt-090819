970fbfa523ec1f13a585686b34eea367703dc515
Charity Parks <tuppercharity@gmail.com>
2019-10-16 23:46:36 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Patient
     attr_accessor :name
 
@@ -26,4 +27,31 @@ class Patient
 
 
 
+=======
+class Patient 
+ 
+  attr_accessor :name
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
+  
+  def appointments 
+    appointments.all.select {|appointment| appointment.patient == self}
+  end
+    
+  def doctors
+    appointments.collect {|apointment| appointment.doctor}
+  end
+>>>>>>> d32ed8255625fceaa8462a2d315af6a7c0b8dad1
 end
